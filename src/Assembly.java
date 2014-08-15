2222366d6976c02b17cf1e7fe1c8d867a7eef7ae
Alejandro Huerta <ahuerta0686@gmail.com>
2014-08-15 11:28:08 -0500

    Added method to grab open glues in assembly.java -alex

--- src/Assembly.java
+++ src/Assembly.java
@@ -8,9 +8,14 @@ public class Assembly {
     // tile system, it can be changed so it needs its own class
     private TileSystem tileSystem;
     // placeholder for the grid
-    private Object Grid;
+    private HashMap<String, Tile> Grid = new HashMap();
     // frontier list: calculated, increased, decreased, and changed here.
     private List<PolyTile> frontier = new ArrayList<PolyTile>();
+
+    HashMap<String, String> openNorthGlues = new HashMap();
+    HashMap<String, String> openEastGlues = new HashMap();
+    HashMap<String, String> openSouthGlues = new HashMap();
+    HashMap<String, String> openWestGlues = new HashMap();
 
     public Assembly(){
         System.out.print("in assembly,");
@@ -20,7 +25,22 @@ public class Assembly {
     }
 
     private void getOpenGlues() {
-        for(PolyTile p : )
+        openNorthGlues.clear();
+        for(Map.Entry<String, Tile> t : Grid.entrySet()) {
+            String[] glueLabels = t.getValue().getGlueLabels();
+            if(!glueLabels[0].equals("")) {
+                openNorthGlues.put(t.getKey(), glueLabels[0]);
+            }
+            if(!glueLabels[1].equals("")) {
+                openEastGlues.put(t.getKey(), glueLabels[1]);
+            }
+            if(!glueLabels[2].equals("")) {
+                openSouthGlues.put(t.getKey(), glueLabels[2]);
+            }
+            if(!glueLabels[3].equals("")) {
+                openWestGlues.put(t.getKey(), glueLabels[3]);
+            }
+        }
     }
 
     // calculate frontier
