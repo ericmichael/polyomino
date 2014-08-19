0c8eb0a4e6d746c77933e3b8cad98a8cbf0c01c8
Dominic Fernandez <dom.ard.fern@gmail.com>
2014-08-19 13:37:24 -0500

    Drawer.java: new static class for drawing stuff. TestCanvasFrame: width/height parameters for panel sizes and other dimensions TestCanvas: now using methods from Drawer class

---
+++ src/com/asarg/polysim/Drawer.java
@@ -0,0 +1,42 @@
+package com.asarg.polysim;
+
+import java.awt.*;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Created by Dom on 8/19/2014.
+ */
+public class Drawer {
+
+    //clears given graphics object, clip must have been set
+    public static  void clearGraphics(Graphics2D g)
+    {
+        Rectangle bounds = g.getClipBounds();
+        g.setComposite(AlphaComposite.Clear);
+        g.drawRect(0,0, bounds.width, bounds.height);
+        g.setComposite(AlphaComposite.SrcOver);
+    }
+
+    public static class TileDrawer
+    {
+        public static void drawTile(Graphics2D g, Tile tile, int x, int y, int diameter)
+        {
+            int colorInt = Integer.parseInt(tile.getColor(), 16);
+            g.setColor(new Color(colorInt >> 16, (colorInt & 0x00FF00) >> 8, colorInt & 0x0000FF));
+
+            g.fillRect(x,y, diameter, diameter);
+
+        }
+        public static void drawTiles(Graphics2D g, Set<Map.Entry<Point, Tile>> tiles, int diameter, Point offset)
+        {
+
+            for(Map.Entry<Point, Tile> mep : tiles) {
+                Point pt = mep.getKey();
+                Tile tile = mep.getValue();
+                drawTile(g,tile, pt.x * diameter + offset.x - diameter / 2,-pt.y * diameter + offset.y - diameter / 2, diameter );
+            }
+        }
+    }
+
+}
--- src/com/asarg/polysim/TestCanvasFrame.java
+++ src/com/asarg/polysim/TestCanvasFrame.java
@@ -8,6 +8,7 @@ import javax.swing.border.Border;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
 import java.util.HashMap;
 
 /**
@@ -16,33 +17,34 @@ import java.util.HashMap;
 public class TestCanvasFrame extends JFrame {
 
     TestCanvas tc;
+    JMenuBar tcfBar = new JMenuBar();
+    JMenuItem nextStepMI = new JMenuItem("Step >");
 
-    TestCanvasFrame()
+    TestCanvasFrame(int w, int h)
     {
         setLayout(new BorderLayout());
-        tc = new TestCanvas();
+        tc = new TestCanvas(w,h);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        JPanel frontierCanvas = new JPanel()
-        {
-            @Override
-            public Dimension getPreferredSize()
-            {
 
-                return new Dimension(1280,200);
-            }
-        };
-      //  add(frontierCanvas, BorderLayout.NORTH);
-        add(tc, BorderLayout.CENTER);
-      //  JButton stepForwards = new JButton("Step >");
-      // add(stepForwards);
+        FrontierCanvas frontierCanvas = new FrontierCanvas(w,h);
+        add(frontierCanvas, BorderLayout.NORTH);
+        add(tc, BorderLayout.SOUTH);
 
         ActionListener al = new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                if(e.getSource().equals(nextStepMI))
+                {
+
+                    System.out.println("yo");
+                }
 
             }
         };
 
+        nextStepMI.addActionListener(al);
+        tcfBar.add(nextStepMI);
+        setJMenuBar(tcfBar);
         pack();
     }
 
@@ -55,6 +57,55 @@ public class TestCanvasFrame extends JFrame {
     public void drawGrid(HashMap<Point, Tile> hmpt)
     {
         tc.drawGrid(hmpt);
+        repaint();
+
+    }
+
+
+
+    class FrontierCanvas extends JPanel
+    {
+        Dimension frameRes = new Dimension();
+        Dimension myRes = new Dimension();
+        BufferedImage frontierBFI;
+        Graphics2D frontierGFX;
+        FrontierCanvas(int w, int h)
+        {
+            myRes.setSize(w, h/5);
+            frontierBFI = new BufferedImage(myRes.width, myRes.height, BufferedImage.TYPE_INT_ARGB);
+            frontierGFX = frontierBFI.createGraphics();
+            frontierGFX.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+        }
+
+
+
+
+        @Override
+        public Dimension getPreferredSize()
+        {
+            return new Dimension(myRes.width, myRes.height);
+        }
+        @Override
+        public void paintComponent(Graphics g)
+        {
+            Graphics2D g2 = (Graphics2D)g;
+            g2.drawImage(frontierBFI,0,0,null);
+
+
+        }
+
+        public void drawFrontier()
+        {
+
+            frontierGFX.setComposite(AlphaComposite.Clear);
+            frontierGFX.fillRect(0,0, myRes.width, myRes.height);
+            frontierGFX.setComposite(AlphaComposite.SrcOver);
+
+
+
+        }
+
 
     }
 
