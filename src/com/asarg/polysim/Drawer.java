a2863c4930396c509aa159c2cc6c7c64c7dc8036
Dominic Fernandez <dom.ard.fern@gmail.com>
2014-08-19 14:27:29 -0500

    Drawer.java: Added some fanciness to tiles drawn. TestCanvas.java:  alpha compsite for canvas gfx set to src

--- src/com/asarg/polysim/Drawer.java
+++ src/com/asarg/polysim/Drawer.java
@@ -1,10 +1,12 @@
 package com.asarg.polysim;
 
 import java.awt.*;
+import java.awt.geom.Rectangle2D;
 import java.util.Map;
 import java.util.Set;
 
 public class Drawer {
+
 
     //clears given graphics object, clip must have been set
     public static  void clearGraphics(Graphics2D g)
@@ -12,17 +14,21 @@ public class Drawer {
         Rectangle bounds = g.getClipBounds();
         g.setComposite(AlphaComposite.Clear);
         g.drawRect(0,0, bounds.width, bounds.height);
-        g.setComposite(AlphaComposite.SrcOver);
+        g.setComposite(AlphaComposite.Src);
     }
 
     public static class TileDrawer
     {
         public static void drawTile(Graphics2D g, Tile tile, int x, int y, int diameter)
         {
+            Rectangle2D labelBounds = g.getFont().getStringBounds(tile.getLabel(), g.getFontRenderContext());
             int colorInt = Integer.parseInt(tile.getColor(), 16);
-            g.setColor(new Color(colorInt >> 16, (colorInt & 0x00FF00) >> 8, colorInt & 0x0000FF));
-
+            g.setColor(new Color(colorInt >> 16, (colorInt & 0x00FF00) >> 8, colorInt & 0x0000FF, 100));
             g.fillRect(x,y, diameter, diameter);
+            g.setColor(Color.black);
+            g.setStroke(new BasicStroke(diameter/20));
+            g.drawRect(x,y,diameter,diameter);
+            g.drawString(tile.getLabel(), x + diameter/2 - (int)labelBounds.getWidth()/2, y + diameter/2 + (int)labelBounds.getHeight()/2);
 
         }
         public static void drawTiles(Graphics2D g, Set<Map.Entry<Point, Tile>> tiles, int diameter, Point offset)
