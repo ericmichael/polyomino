fbf39ebbff7f9878923c40d52358d26f665b8d46
Eric Martinez <eric.michael.mtz@gmail.com>
2014-08-19 20:04:21 -0500

    Refactored into Tetris example into TetrisSimulation class, Added backwards and forwards stepping through simulation environment.

--- src/com/asarg/polysim/Assembly.java
+++ src/com/asarg/polysim/Assembly.java
@@ -20,6 +20,8 @@ public class Assembly {
     public HashMap<Point, Tile> Grid = new HashMap<Point, Tile>();
     // frontier list: calculated, increased, decreased, and changed here.
     private List<Pair<Point, PolyTile>> frontier = new ArrayList<Pair<Point, PolyTile>>();
+    private List<Pair<Point, PolyTile>> attached = new ArrayList<Pair<Point, PolyTile>>();
+
 
     //Open glue ends stored by their coordinate
     HashMap<Point, String> openNorthGlues = new HashMap<Point, String>();
@@ -73,6 +75,17 @@ public class Assembly {
             Point tmp = new Point(t.getLocation());
             tmp.translate(x, y);
             Grid.put(tmp, t);
+        }
+    }
+
+    private void removePolytile(PolyTile p, int x, int y) {
+        for(Tile t : p.tiles) {
+            Point tmp = new Point(t.getLocation());
+            tmp.translate(x, y);
+            Tile existing = Grid.get(tmp);
+            if(existing.samePolyTile(p)) {
+                Grid.remove(tmp);
+            }else System.out.println("not removing polytile");
         }
     }
 
@@ -154,7 +167,6 @@ public class Assembly {
         fillPossibleList(t, EAST);
         fillPossibleList(t, SOUTH);
         fillPossibleList(t, WEST);
-        System.out.println(possibleAttach);
     }
     // calculate frontier
 
@@ -245,6 +257,7 @@ public class Assembly {
         Pair<Point, PolyTile> x = frontier.get(rn.nextInt(frontier.size()));
         placePolytile(x.getValue(), x.getKey().x, x.getKey().y );
         frontier.remove(x);
+        attached.add(x);
         return x.getValue().getConcentration();
     }
 
@@ -263,6 +276,15 @@ public class Assembly {
         cleanUp();
         getOpenGlues();
         return r;
+    }
+
+    public void detach(){
+        if(!attached.isEmpty()) {
+            Pair<Point, PolyTile> last = attached.remove(attached.size() - 1);
+            removePolytile(last.getValue(), last.getKey().x, last.getKey().y);
+            cleanUp();
+            getOpenGlues();
+        }
     }
 
     //Add "random" polytile from frontier based on Polytile's concentrations
---
+++ src/com/asarg/polysim/TetrisSimulation.java
@@ -0,0 +1,138 @@
+package com.asarg.polysim;
+
+import java.util.Random;
+
+public class TetrisSimulation {
+
+    public static String[] blankGlues() {
+        String label[] = new String[4];
+        label[0] = null;
+        label[1] = null;
+        label[2] = null;
+        label[3] = null;
+        return label;
+    }
+
+    public static PolyTile tetrisU() {
+        String blank[] = blankGlues();
+        String glue[] = {"e", null, null, null};
+
+        PolyTile poly = new PolyTile("U");
+
+        poly.setColor("008299");
+
+        poly.addTile(0, 0, glue);
+        poly.addTile(-1, 0, blank);
+        poly.addTile(-1, 1, blank);
+        poly.addTile(1, 0, blank);
+        poly.addTile(1, 1, blank);
+
+        return poly;
+    }
+
+    public static PolyTile tetrisV() {
+        String blank[] = blankGlues();
+        String glue[] = {null, null, null, "a"};
+
+        PolyTile poly = new PolyTile("V");
+
+        poly.setColor("2672EC");
+
+        poly.addTile(0, 0, blank);
+        poly.addTile(-1, 0, blank);
+        poly.addTile(-1, 1, blank);
+        poly.addTile(-1, 2, glue);
+        poly.addTile(1, 0, blank);
+
+        return poly;
+    }
+
+    public static PolyTile tetrisX() {
+        String blank[] = blankGlues();
+        String glue[] = {null, null, "e", null};
+        String glue2[] = {null, null, null, "d"};
+
+        PolyTile poly = new PolyTile("X");
+
+        poly.setColor("8C0095");
+
+        poly.addTile(0, 0, blankGlues());
+        poly.addTile(-1, 0, blankGlues());
+        poly.addTile(1, 0, blankGlues());
+        poly.addTile(0, 1, glue2);
+        poly.addTile(0, -1, glue);
+
+        return poly;
+    }
+
+    public static PolyTile tetrisF() {
+        String[] gleft = {null, null, null, "b"};
+        String[] gright = {null, "d", null, null};
+
+        PolyTile tetrisF = new PolyTile("F");
+
+        tetrisF.setColor("5133AB");
+
+        tetrisF.addTile(0, 0, blankGlues());
+        tetrisF.addTile(0, -1, blankGlues());
+        tetrisF.addTile(-1, 0, blankGlues());
+        tetrisF.addTile(0, 1, gleft);
+        tetrisF.addTile(1, 1, gright);
+        return tetrisF;
+    }
+
+    public static PolyTile tetrisI() {
+        String[] gtop = {null, "a", null, null};
+        String[] gbottom = {null, "b", null, null};
+
+        PolyTile tetris = new PolyTile("I");
+
+        tetris.setColor("AC193D");
+
+        tetris.addTile(0, 0, blankGlues());
+        tetris.addTile(0, 1, blankGlues());
+        tetris.addTile(0, -1, blankGlues());
+        tetris.addTile(0, 2, gtop);
+        tetris.addTile(0, -2, gbottom);
+        return tetris;
+    }
+
+    public static PolyTile tetrisL() {
+        String[] gtop = {null, "c", null, "a"};
+
+        PolyTile tetris = new PolyTile("L");
+
+        tetris.setColor("D24726");
+
+        tetris.addTile(0, 0, blankGlues());
+        tetris.addTile(0, 1, blankGlues());
+        tetris.addTile(0, 2, blankGlues());
+        tetris.addTile(0, 3, gtop);
+        tetris.addTile(1, 0, blankGlues());
+        return tetris;
+    }
+
+    public TileSystem ts;
+    public Assembly assembly;
+
+    public TetrisSimulation(int temperature){
+        ts = new TileSystem(temperature);
+
+        ts.addPolyTile(tetrisF());
+        ts.addPolyTile(tetrisI());
+        ts.addPolyTile(tetrisL());
+        ts.addPolyTile(tetrisU());
+        ts.addPolyTile(tetrisV());
+        ts.addPolyTile(tetrisX());
+
+        ts.addGlueFunction("a","a",2);
+        ts.addGlueFunction("b","b",2);
+        ts.addGlueFunction("c","c",2);
+        ts.addGlueFunction("d","d",2);
+        ts.addGlueFunction("e","e",2);
+        ts.addGlueFunction("f","f",2);
+
+        assembly = new Assembly(ts);
+        assembly.placeSeed(tetrisF());
+    }
+}
