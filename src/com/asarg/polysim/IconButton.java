09ed200ee2b0160fc3969871f4632ab64dd7a139
Dominic Fernandez <dom.ard.fern@gmail.com>
2014-08-22 13:49:04 -0500

    had issue where the buttons were stealing focus from the assembly panel, and therefore was unable to listen for key events, I disabled focusing on these buttons

--- src/com/asarg/polysim/IconButton.java
+++ src/com/asarg/polysim/IconButton.java
@@ -10,11 +10,14 @@ import java.io.IOException;
 
 public class IconButton extends JButton {
     public IconButton() {
+        setFocusable(false);
         try {
             InputStream in = this.getClass().getResourceAsStream("/fontawesome.ttf");
             Font ttfBase = Font.createFont(Font.TRUETYPE_FONT, in);
             Font ttfReal = ttfBase.deriveFont(Font.BOLD, 24);
             setFont(ttfReal);
+
+
         } catch (FontFormatException e) {
             e.printStackTrace();
         } catch (IOException e) {
